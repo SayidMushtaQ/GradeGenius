8e9627f13e7990866f95d05399cf739ffa4ca1e6
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-15 19:56:02 +0530

    Initail structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div>a</div>;
+  return (
+    <div className="container">
+      <div className="sub-container">
+        <div className="main-box">
+          <section className="box-header">
+            <header>
+              <h1>Grades with GradeGenius</h1>
+              <p>
+                Simple web app for quick and accurate exam grade calculations,
+                designed to help students and educators effortlessly track
+                academic progress.
+              </p>
+            </header>
+          </section>
+          <span>Grades</span>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
