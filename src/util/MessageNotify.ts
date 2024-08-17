55f6c8196271b31360ffd42eb42c150c63fd236d
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-17 21:42:56 +0530

    React Notify

---
+++ src/util/MessageNotify.ts
@@ -0,0 +1,26 @@
+import { toast } from 'react-toastify';
+export const ErrorNotify = (message:string)=>{
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
+
+export const SuccessNotify = (message:string)=>{
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
